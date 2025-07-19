d8f02a8274d1315d2046f698c3a47f01c82db82c
spike04 <rubinbajracharya@yahoo.com>
2025-07-19 11:48:32 +0545

    chor: puter setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import Navbar from '@/components/Navbar'
-import type { Route } from './+types/home'
-import { resumes } from '@/constants'
 import ResumeCard from '@/components/ResumeCard'
+import { resumes } from '@/constants'
+import { usePuterStore } from '@/lib/puter'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { isLoading, auth } = usePuterStore()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate('/auth?next=/')
+    }
+  }, [auth.isAuthenticated])
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
